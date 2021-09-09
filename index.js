ea507854c107b22ba960a5cea305b43ecaffaf21
Yuval Starnes <yuval@sensusmd.com>
2021-09-09 16:30:55 +0300

    device names to client

--- index.js
+++ index.js
@@ -1,69 +1,109 @@
 const usb = require('usb')
 const io = require("socket.io")(3000);
 
+(async () => {
 
-io.on("connection", socket => {
+
+io.on("connection", async socket => {
 
     socket.emit("Connected!");
 
 
+    usb.on('attach', (device) => {
+        (async () => {
+            const tree = await makeUSBTree()
+            socket.emit("tree", tree);
+        })();
+    });
 
-usb.on('attach', (device) => {
-
-   const tree = makeUSBTree()
-    socket.emit("tree",tree);
-});
-
-usb.on('detach', (device) => {
-
-  const tree = makeUSBTree()
-    socket.emit("tree",tree);
-});
-
-
-function makeUSBTree() {
-
-  const  usbTree = []
-     let usbList = usb.getDeviceList()
+    usb.on('detach', (device) => {
+        (async () => {
+            const tree = await makeUSBTree()
+            socket.emit("tree", tree);
+        })();
+    });
 
 
-    usbList.forEach((item) => {
-        item.parent
-    })
+    async function makeUSBTree() {
 
-    let deviceList = usbList.map(a => {return {...a}})
-
-    const idMapping = deviceList.reduce((acc, el, i) => {
-        acc[el.deviceDescriptor.idProduct] = i;
-        return acc;
-    }, {});
+        const usbTree = []
+        let usbList = usb.getDeviceList()
 
 
-    deviceList.map(el => {
-        if (el._parent === null) {
+        usbList.forEach((item) => {
+            item.parent
+        })
+
+        let deviceList = await descriptorLoop(usbList)
+
+
+        deviceList.map(el => {
+            if (el._parent === null) {
+                const obj = {
+                    id: el.deviceDescriptor.idProduct,
+                    parentId: null,
+                    label: ` Product: ${el.product}, Vendor: ${el.manufacturer}`
+                }
+                usbTree.push(obj);
+                return;
+            }
             const obj = {
-                id : el.deviceDescriptor.idProduct,
-                parentId : null,
-                label: ` Product: ${el.deviceDescriptor.idProduct}, Vendor: ${el.deviceDescriptor.idVendor}`
+                id: el.deviceDescriptor.idProduct,
+                parentId: el._parent.deviceDescriptor.idProduct,
+                label: ` Product: ${el.product}, Vendor: ${el.manufacturer}`
             }
             usbTree.push(obj);
-            return;
-        }
-        const obj = {
-            id : el.deviceDescriptor.idProduct,
-            parentId : el._parent.deviceDescriptor.idProduct,
-            label: ` Product: ${el.deviceDescriptor.idProduct}, Vendor: ${el.deviceDescriptor.idVendor}`
-        }
-        usbTree.push(obj);
-        el.id = el.deviceDescriptor.idProduct
-        el.parentId = el._parent.deviceDescriptor.idProduct
-    });
-    return usbTree
+            el.id = el.deviceDescriptor.idProduct
+            el.parentId = el._parent.deviceDescriptor.idProduct
+        });
+        return usbTree
+    }
+
+    const tree = await makeUSBTree()
+
+    socket.emit("tree", tree);
+    console.log()
+});
+
+})();
+
+async function descriptorLoop(usbList){
+    for await (let device of usbList) {
+        const manufacturer = await getManufacturer(device)
+        const product = await getProduct(device)
+        device.manufacturer = manufacturer
+        device.product = product
+    }
+
+    return usbList.map(a => {return {...a}})
+
 }
 
-const tree = makeUSBTree()
+async function getProduct(device){
+   return new Promise((resolve, reject) => {
+       device.open()
+       device.getStringDescriptor(device.deviceDescriptor.iProduct,  (err, product)=> {
+         if(err){
+             reject(err)
+         } else {
+             resolve(product)
+             device.close()
+         }
+       })
+    });
+}
 
-socket.emit("tree",tree);
-console.log()
+async function getManufacturer(device){
+    return new Promise((resolve, reject) => {
+        device.open()
+        device.getStringDescriptor(device.deviceDescriptor.iManufacturer,  (err, manufacturer)=> {
+            if(err){
+                reject(err)
+            } else {
+                resolve(manufacturer)
+                device.close()
+            }
+        })
+    });
+}
 
-});
