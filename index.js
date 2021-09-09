842afcdf72f128c98a390ccd3c46112d68da4457
Yuval Starnes <yuval@sensusmd.com>
2021-09-09 04:20:20 +0300

    data struct fit to client socket.io

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const usb = require('usb')
+const io = require("socket.io")(3000);
+
+
+io.on("connection", socket => {
+
+    socket.emit("Connected!");
 
 
 
 usb.on('attach', (device) => {
 
    const tree = makeUSBTree()
-    console.log()
+    socket.emit("tree",tree);
 });
 
 usb.on('detach', (device) => {
 
   const tree = makeUSBTree()
-    console.log()
+    socket.emit("tree",tree);
 });
 
 
@@ -35,16 +41,29 @@ function makeUSBTree() {
 
     deviceList.map(el => {
         if (el._parent === null) {
-            usbTree.push(el);
+            const obj = {
+                id : el.deviceDescriptor.idProduct,
+                parentId : null,
+                label: ` Product: ${el.deviceDescriptor.idProduct}, Vendor: ${el.deviceDescriptor.idVendor}`
+            }
+            usbTree.push(obj);
             return;
         }
-        const parentEl = deviceList[idMapping[el._parent.deviceDescriptor.idProduct]];
-
-        parentEl.children = [...(parentEl.children || []), el];
+        const obj = {
+            id : el.deviceDescriptor.idProduct,
+            parentId : el._parent.deviceDescriptor.idProduct,
+            label: ` Product: ${el.deviceDescriptor.idProduct}, Vendor: ${el.deviceDescriptor.idVendor}`
+        }
+        usbTree.push(obj);
+        el.id = el.deviceDescriptor.idProduct
+        el.parentId = el._parent.deviceDescriptor.idProduct
     });
     return usbTree
 }
 
 const tree = makeUSBTree()
+
+socket.emit("tree",tree);
 console.log()
 
+});
